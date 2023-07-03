3b7c324e773d9d0c3700065fdaf6979605ac6e7f
Chinmay Prashanth <85664232+thegiant007@users.noreply.github.com>
2023-07-03 20:41:07 +0530

    2.cc

--- Part B/2.cc
+++ Part B/2.cc
@@ -81,3 +81,13 @@ AnimationInterface anim ("second.xml");
   Simulator::Destroy ();
   
 }
+cd ns-allinone-3.30.1/ns-3.30.1
+  cd scratch/
+  gedit filename.cc
+  cd  ..
+  ./waf
+  ./waf  --run scratch/filename 
+  cd  ..
+  cd netanim-3.108/
+  export QT_X11_NO_MITSHM=1 
+  ./NetAnim
