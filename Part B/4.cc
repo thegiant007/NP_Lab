33aac6cab78c17fe4461c24728fd8adcf31ee0a5
Chinmay Prashanth <85664232+thegiant007@users.noreply.github.com>
2023-07-03 20:41:50 +0530

    4.cc

--- Part B/4.cc
+++ Part B/4.cc
@@ -48,3 +48,13 @@ anim.EnablePacketMetadata(true);
   
 
   }
+cd ns-allinone-3.30.1/ns-3.30.1
+  cd scratch/
+  gedit filename.cc
+  cd  ..
+  ./waf
+  ./waf  --run scratch/filename 
+  cd  ..
+  cd netanim-3.108/
+  export QT_X11_NO_MITSHM=1 
+  ./NetAnim
